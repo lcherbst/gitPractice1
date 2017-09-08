722fb3e43501e76f32aee2e25abfdf99d499ddda
Lee Herbst <admin@Lees-iMac.local>
2017-09-08 10:10:07 -0400

    Initial Commit

---
+++ gitPractice1/gitPractice1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gitPractice1
+//
+//  Created by Lee Herbst on 9/8/17.
+//  Copyright © 2017 Lee Herbst. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
